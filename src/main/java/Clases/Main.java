3801fb86449bf2bd384172b282bc7ccdbfa14974
Antonioquiros <antonioquiros2324@gmail.com>
2025-01-30 21:13:50 +0100

    Creacion swtich-case

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -2,6 +2,8 @@ package Clases;
 
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 /*
     Hacer un menu que abra las siguientes aplicacones Steam, Google Chrome, Visual Studio Code y Spotify.
     Emilio hará Spotify y Visual Studio Code y Antonio hará Steam y Google Chrome
@@ -10,8 +12,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int opcion;
-        do{
+        String opcion = "";
+        Main main = new Main();
+        while (opcion != "0")
+        {
             System.out.println("---------------------------------");
             System.out.println("Menu de aplicaciones");
             System.out.println("1. Spotify");
@@ -21,7 +25,36 @@ public class Main {
             System.out.println("0. Salir");
             System.out.println("---------------------------------");
             System.out.println("Elige una aplicacion: ");
-             opcion = sc.nextInt();
-        }while (opcion != 0);
+             opcion = sc.nextLine();
+
+
+             switch (opcion){
+
+                 case "1":
+                     main.abrirSpotify();
+                     System.out.println("Abriendo spotify");
+                     break;
+                 case "2":
+                     main.abrirVisualStudioCode();
+                     break;
+                 case "3":
+                     main.abrirGoogleChrome();
+                     break;
+                 case "4":
+                     main.abrirSteam();
+                     break;
+                 case "0":
+                     System.out.println("Saliendo del programa...");
+                     System.exit(0);
+                     break;
+                 default:
+                     System.out.println("La opcion introducida no es valida");
+                     break;
+             }
+        }
     }
+    private void abrirSpotify(){}
+    private void abrirSteam(){}
+    private void abrirGoogleChrome(){}
+    private void abrirVisualStudioCode(){}
 }

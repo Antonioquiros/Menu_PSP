b36a161b9861e86be4760dc99e41a16ac11ef15d
Antonioquiros <antonioquiros2324@gmail.com>
2025-01-30 20:39:52 +0100

    Expliacion del ejercicio

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -1,6 +1,9 @@
 package Clases;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+/*
+    Hacer un menu que abra las siguientes aplicacones Steam, Google Chrome, Visual Studio Code y Spotify.
+    Emilio hará Spotify y Visual Studio Code y Antonio hará Steam y Google Chrome
+ */
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {

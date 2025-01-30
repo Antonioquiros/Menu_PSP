2bb4b4e99b69a4f40e9614fef36dd3166d7adbd2
Antonioquiros <antonioquiros2324@gmail.com>
2025-01-30 20:45:45 +0100

    Creacion del menu

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -1,5 +1,7 @@
 package Clases;
 
+import java.util.Scanner;
+
 /*
     Hacer un menu que abra las siguientes aplicacones Steam, Google Chrome, Visual Studio Code y Spotify.
     Emilio hará Spotify y Visual Studio Code y Antonio hará Steam y Google Chrome
@@ -7,9 +9,19 @@ package Clases;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Paco Pepe Chema Chema Pepe Paco");
-
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do{
+            System.out.println("---------------------------------");
+            System.out.println("Menu de aplicaciones");
+            System.out.println("1. Spotify");
+            System.out.println("2. Visual Studio Code");
+            System.out.println("3. Google Chrome");
+            System.out.println("4. Steam");
+            System.out.println("0. Salir");
+            System.out.println("---------------------------------");
+            System.out.println("Elige una aplicacion: ");
+             opcion = sc.nextInt();
+        }while (opcion != 0);
     }
 }
